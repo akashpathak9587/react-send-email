06dfb8a938f67247c05a86b4aa3cc797db011402
Akash Pathak <aakapa95@gmail.com>
2025-01-14 19:57:01 +0530

    docker setup done for production

--- src/app.ts
+++ src/app.ts
@@ -16,5 +16,5 @@ app.use(express.json());
 app.use("/v1", commonRouter);
 
 app.listen(process.env.PORT, () => {
-  console.log("Server is running on port 3000");
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
